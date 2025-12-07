58041f27306c960a2868da05c6aff43da2bf3704
Toshihito Kon <toshihito.kon@repro.io>
2025-12-07 16:46:26 +0900

    add: fixit comment and ignore eslint

--- front/src/lib/components/Button.svelte
+++ front/src/lib/components/Button.svelte
@@ -7,14 +7,25 @@
     class?: string;
     disabled?: boolean;
     href?: string;
+    // FIXIT: Don't use any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any;
   }
 
-  let { variant = 'primary', size = 'md', class: className = '', children, ...restProps }: Props = $props();
+  let {
+    variant = 'primary',
+    size = 'md',
+    class: className = '',
+    children, 
+    // FIXIT: https://svelte.dev/docs/svelte/compiler-warnings#custom_element_props_identifier
+    // eslint-disable-next-line svelte/valid-compile
+    ...restProps
+  }: Props = $props();
 
   const variantClasses = {
     primary: 'bg-[#E86B79] hover:bg-[#d55a6a] text-white rounded transition-colors',
-    secondary: 'bg-white hover:bg-gray-50 text-gray-600 border border-gray-300 rounded transition-colors',
+    secondary:
+      'bg-white hover:bg-gray-50 text-gray-600 border border-gray-300 rounded transition-colors',
     danger: 'text-red-600 hover:text-red-700 transition-colors',
     link: 'text-[#E86B79] hover:text-[#d55a6a] transition-colors'
   };
@@ -31,4 +42,3 @@
 <BitsButton.Root class={computedClass} {...restProps}>
   {@render children?.()}
 </BitsButton.Root>
-

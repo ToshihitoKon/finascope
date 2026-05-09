1351c9e3f87147b0f6f5da3bf30771be3b3dfb6e
Toshihito Kon <toshihito.kon@repro.io>
2026-05-09 15:47:55 +0900

    test(api): translate exceptions spec descriptions to English (#48)

--- api/spec/lib/exceptions_spec.rb
+++ api/spec/lib/exceptions_spec.rb
@@ -4,44 +4,44 @@ require "spec_helper"
 require "lib/exceptions"
 
 RSpec.describe Exceptions do
-  describe "クラス階層" do
-    it "InternalServerError は StandardError を継承する" do
+  describe "class hierarchy" do
+    it "InternalServerError inherits from StandardError" do
       expect(Exceptions::InternalServerError.ancestors).to include(StandardError)
     end
 
-    it "InvalidArgument は StandardError を継承する" do
+    it "InvalidArgument inherits from StandardError" do
       expect(Exceptions::InvalidArgument.ancestors).to include(StandardError)
     end
 
-    it "NotFound は StandardError を継承する" do
+    it "NotFound inherits from StandardError" do
       expect(Exceptions::NotFound.ancestors).to include(StandardError)
     end
 
-    it "Unauthorized は StandardError を継承する" do
+    it "Unauthorized inherits from StandardError" do
       expect(Exceptions::Unauthorized.ancestors).to include(StandardError)
     end
   end
 
-  describe "raise の挙動" do
-    it "メッセージ付きで raise できる" do
+  describe "raise behavior" do
+    it "can be raised with a message" do
       expect { raise Exceptions::NotFound, "missing" }
         .to raise_error(Exceptions::NotFound, "missing")
     end
 
-    it "引数なしでも raise できる" do
+    it "can be raised without arguments" do
       expect { raise Exceptions::InvalidArgument }
         .to raise_error(Exceptions::InvalidArgument)
     end
 
-    it "型による rescue が機能する（別クラスでは捕捉されない）" do
+    it "is not caught by rescue clause for a different class" do
       expect do
         raise Exceptions::InvalidArgument, "bad"
       rescue Exceptions::NotFound
-        # 捕捉されない想定
+        # not expected to be caught here
       end.to raise_error(Exceptions::InvalidArgument)
     end
 
-    it "型による rescue が機能する（同クラスで捕捉される）" do
+    it "is caught by rescue clause for the same class" do
       caught = nil
       begin
         raise Exceptions::Unauthorized, "no token"
